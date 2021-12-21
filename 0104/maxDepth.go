ac1348a24480c854a1956b1df7817f4b9c1c0571
张春雷 <zhangchunlei@qiyi.com>
2021-12-21 13:54:53 +0800

    update 0104

---
+++ 0104/maxDepth.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import "github.com/bbs731/leetcode/structures"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode = structures.TreeNode
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
