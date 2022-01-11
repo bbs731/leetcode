e59fdf0d471f136ab03c982be8af78800e812a49
张春雷 <zhangchunlei@qiyi.com>
2022-01-11 16:21:02 +0800

    update 0376

---
+++ 0376/wiggle.go
@@ -0,0 +1,46 @@
+package leetcode
+
+func wiggleMaxLength(nums []int) int {
+	N := len(nums)
+	prev := make([]int, N)
+	direction := make([]int, N) // 0 stands for unknown, 1 stands for increase from prev element, -1 stands for decrease from prev statement
+	length := make([]int, N)
+
+	prev[0] = -1
+	direction[0] = 0
+	length[0] = 1
+
+	ans := 1
+	for i := 1; i < N; i++ {
+		prev[i] = -1
+		direction[i] = 0
+		length[i] = 1
+		maxlength := 0
+		for j := i - 1; j >= 0; j-- {
+			if nums[i] > nums[j] && direction[j] != 1 {
+				if length[j]+1 > maxlength {
+					maxlength = length[j] + 1
+					prev[i] = j
+					direction[i] = 1
+					length[i] = maxlength
+				}
+			} else if nums[i] < nums[j] && direction[j] != -1 {
+				if length[j]+1 > maxlength {
+					maxlength = length[j] + 1
+					prev[i] = j
+					direction[i] = -1
+					length[i] = maxlength
+				}
+			} // nums[i] == nums[j] 这种情况， 在初始化 prev[i], direction[i], length[i] 的时候，已经 cover 住了
+		}
+		ans = max(ans, maxlength)
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
