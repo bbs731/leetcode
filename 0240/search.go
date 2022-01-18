c095011f9da697c7a1ab76f9de5239fcf5f6678b
张春雷 <zhangchunlei@qiyi.com>
2022-01-18 09:54:58 +0800

    update offer

---
+++ 0240/search.go
@@ -0,0 +1,43 @@
+package leetcode
+
+func binarySearch(nums []int, target int) bool {
+	lo := 0
+	hi := len(nums) - 1
+
+	for lo <= hi {
+		mid := lo + (hi-lo+1)>>1
+		if nums[mid] == target {
+			return true
+
+		} else if nums[mid] > target {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return false
+}
+
+func search(matrix [][]int, row, column, M, N, target int) bool {
+	if row > M || column > N {
+		return false
+	}
+
+	if binarySearch(matrix[row][column:N+1], target) {
+		return true
+	}
+	var c []int
+	for i := row; i <= M; i++ {
+		c = append(c, matrix[i][column])
+	}
+	if binarySearch(c, target) {
+		return true
+	}
+	return search(matrix, row+1, column+1, M, N, target)
+}
+
+func searchMatrix(matrix [][]int, target int) bool {
+	M := len(matrix)
+	N := len(matrix[0])
+	return search(matrix, 0, 0, M-1, N-1, target)
+}
