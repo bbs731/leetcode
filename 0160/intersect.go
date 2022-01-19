f69ba62515014d305b6cc8b7ee4ae3c7f1cd2097
张春雷 <zhangchunlei@qiyi.com>
2022-01-19 17:53:12 +0800

    update 0160

---
+++ 0160/intersect.go
@@ -0,0 +1,54 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+/*
+困扰了，你多年的题， 甚至让你到了怀疑人生的地步， 结果，它就是一个简单的题！ 
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	m, n := 1, 1
+
+	posA := headA
+	for posA.Next != nil {
+		m++
+		posA = posA.Next
+	}
+	posB := headB
+	for posB.Next != nil {
+		n++
+		posB = posB.Next
+	}
+
+	// 2 list not intersect
+	if posA != posB {
+		return nil
+	}
+
+	if m < n {
+		steps := n - m
+		for steps > 0 {
+			headB = headB.Next
+			steps--
+		}
+	} else {
+		steps := m - n
+		for steps > 0 {
+			headA = headA.Next
+			steps--
+		}
+	}
+
+	for headA != headB {
+		headA = headA.Next
+		headB = headB.Next
+	}
+
+	return headA
+}
