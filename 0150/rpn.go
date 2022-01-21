d0d2b6556766a49637974e200b13d8f68f6f9f49
张春雷 <zhangchunlei@qiyi.com>
2022-01-21 11:06:07 +0800

    update 0150

---
+++ 0150/rpn.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import "strconv"
+
+/*
+用 stack, 其它的花里胡哨的想法，都不对！ 
+ */
+func evalRPN(tokens []string) int {
+	stack := []int{}
+
+	for i := 0; i < len(tokens); i++ {
+		num, err := strconv.Atoi(tokens[i])
+		if err == nil {
+			stack = append(stack, num)
+		} else {
+			op1 := stack[len(stack)-1]
+			op2 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			if tokens[i] == "+" {
+				stack = append(stack, op1+op2)
+			} else if tokens[i] == "-" {
+				stack = append(stack, op2-op1)
+			} else if tokens[i] == "*" {
+				stack = append(stack, op2*op1)
+			} else if tokens[i] == "/" {
+				stack = append(stack, op2/op1)
+			}
+		}
+	}
+	return stack[0]
+}
