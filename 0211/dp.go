f8e15dba17741eaf1ed4dc2196319cbab125fca5
张春雷 <zhangchunlei@qiyi.com>
2022-03-11 18:36:19 +0800

    211 DP, transition function found is hard

---
+++ 0211/dp.go
@@ -0,0 +1,63 @@
+package leetcode
+
+func maximalSquare(matrix [][]byte) int {
+	M := len(matrix)
+	N := len(matrix[0])
+
+	ans := 0
+	// dp[i][j] 代表， 坐标 i, j 的方格作为正方形的，右下角的正方形的长度.
+	dp := make([][]int, M)
+	for i := 0; i < M; i++ {
+		dp[i] = make([]int, N)
+	}
+
+	//  初始化， first row and first column
+	for i := 0; i < M; i++ {
+		if matrix[i][0] == '1' {
+			dp[i][0] = 1
+			ans = 1
+		}
+	}
+	for j := 0; j < N; j++ {
+		if matrix[0][j] == '1' {
+			dp[0][j] = 1
+			ans = 1
+		}
+	}
+	for i := 0; i < M; i++ {
+		for j := 0; j < N; j++ {
+			if matrix[i][j] == '0' {
+				dp[i][j] = 0
+				continue
+			}
+			dp[i][j] = 1
+			ans = max(ans, 1)
+			if i-1 >= 0 && j-1 >= 0 {
+				// dp[i][j]  和  dp[i-1][j], dp[i][j-1] 还有 dp[i-1][j-1] 有关系， 但是，找到这个关系，太难了！ 哎！
+				// dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1  是这样吗？
+				c := min(dp[i-1][j], dp[i][j-1])
+				if dp[i-1][j-1] >= c {
+					dp[i][j] = max(dp[i][j], c+1)
+				} else {
+					dp[i][j] = max(dp[i][j], c)
+				}
+				ans = max(ans, dp[i][j])
+			}
+		}
+	}
+	return ans * ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
