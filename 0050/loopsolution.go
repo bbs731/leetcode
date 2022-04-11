67e32568a1bc77c0d4db5a4595d3af9422703449
张春雷 <zhangchunlei@qiyi.com>
2022-04-11 17:33:33 +0800

    50 adding template code

--- 0050/loopsolution.go
+++ 0050/loopsolution.go
@@ -39,3 +39,51 @@ func myPow(x float64, n int) float64 {
 		return pow(x, n)
 	}
 }
+
+/*
+
+using ll = long long;
+const int M = 2;
+
+struct Ma
+{
+    int a[M][M];
+    Ma()
+    {
+        memset(a, 0, sizeof(a));
+    }
+
+    void init() // 复位为单位阵
+    {
+        a[0][0] = a[1][1] = 1;
+        a[0][1] = a[1][1] = 0;
+    }
+
+    Ma operator*(const Ma& B) const
+    {
+        Ma ans;
+        for(int i = 0; i < M; ++i)
+            for(int j = 0; j < M; ++j)
+                for(int k = 0; k < M; ++k)
+                    ans.a[i][j] += a[i][k] * B.a[k][j];
+        return ans;
+    }
+
+    Ma operator^(int n) const
+    {
+        Ma ans;
+        ans.init();
+        Ma A = *this; // 拷贝一个出来用于自乘
+        while(n)
+        {
+            if(n & 1)
+                ans = ans * A;
+            A = A * A;
+            n >>= 1;
+        }
+        return ans;
+    }
+};
+
+
+ */
