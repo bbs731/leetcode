19f3d0acbc4d89a3cdd609a984362e79d27e8b1c
张春雷 <zhangchunlei@qiyi.com>
2022-06-24 11:58:56 +0800

    140 backtrace and trie

---
+++ 0140/backtrace-tie.go
@@ -0,0 +1,76 @@
+package leetcode
+
+type Trie struct {
+	word bool
+	s    string
+	next map[byte]*Trie
+}
+
+func (t *Trie) Insert(w string) {
+	n := len(w)
+	r := t
+	for i := 0; i < n; i++ {
+		if r.next[w[i]] == nil {
+			r.next[w[i]] = &Trie{
+				false,
+				"",
+				make(map[byte]*Trie),
+			}
+		}
+		r = r.next[w[i]]
+		if i == n-1 {
+			r.word = true
+			r.s = w
+		}
+	}
+}
+
+func (t *Trie) Traverse(s string) []string {
+	ans := make([]string, 0)
+	r := t
+	for i := 0; i < len(s); i++ {
+		if r == nil {
+			break
+		}
+		if r.word {
+			ans = append(ans, r.s)
+		}
+		r = r.next[s[i]]
+	}
+	if r != nil && r.word { // 这里，让你困住了好长时间！
+		ans = append(ans, r.s)
+	}
+
+	return ans
+}
+
+func wordBreak(s string, wordDict []string) []string {
+	root := &Trie{
+		false,
+		"",
+		make(map[byte]*Trie),
+	}
+
+	for _, w := range wordDict {
+		root.Insert(w)
+	}
+
+	ans := make([]string, 0)
+	var dfs func(string, string)
+
+	dfs = func(s string, sentence string) {
+
+		if s == "" {
+			ans = append(ans, sentence[1:])
+			return
+		}
+
+		candidates := root.Traverse(s)
+		for _, c := range candidates {
+			dfs(s[len(c):], sentence+" "+c)
+		}
+	}
+	dfs(s, "")
+
+	return ans
+}
