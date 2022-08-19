0b9114238ed8674a0f64ce56f9231b5608f60c11
张春雷 <zhangchunlei@qiyi.com>
2022-08-19 10:45:41 +0800

    adding example code for tarjan algorithm

--- algorithms/graphs/connected compones/edge cut/tarjan.cc
+++ algorithms/graphs/connected compones/edge cut/tarjan.cc
@@ -1,4 +1,12 @@
 
+/*
+割边的定义：
+
+对于一个无向图，如果删掉一条边后图中的连通分量数增加了，则称这条边为桥或者割边。严谨来说，就是：假设有连通图  G= {V, E}，e 是其中一条边（即 e (- E ），如果  是不连通的，则边  G- e 是图 G  的一条割边（桥）。
+
+
+*/
+
 // C++ Version
 int low[MAXN], dfn[MAXN], iscut[MAXN], dfs_clock;
 bool isbridge[MAXN];
